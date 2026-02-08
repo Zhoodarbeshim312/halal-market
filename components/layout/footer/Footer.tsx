f0be2c0706fd701221d7292cd5ffb8f0a67c703e
Zhoodarbeshim <joodarbeshimaktanov@gmail.com>
2026-02-09 01:21:46 +0600

    footer и header адаптирован

--- components/layout/footer/Footer.tsx
+++ components/layout/footer/Footer.tsx
@@ -7,68 +7,103 @@ import {
   FaFacebookF,
   FaInstagram,
   FaLinkedinIn,
+  FaRegUserCircle,
   FaTwitter,
   FaYoutube,
 } from "react-icons/fa";
+import { FiMenu } from "react-icons/fi";
+import { GoHome } from "react-icons/go";
 
 const Footer: FC = () => {
   return (
-    <footer className={scss.Footer}>
-      <div className="container">
-        <div className={scss.content}>
-          <div className={scss.nav}>
-            <h3>Column One</h3>
-            <li>Twenty One</li>
-            <li>Thirty Two</li>
-            <li>Fourty Three</li>
-            <li>Fifty Four</li>
-          </div>
-          <div className={scss.nav}>
-            <h3>Column Two</h3>
-            <li>Sixty Five</li>
-            <li>Seventy Six</li>
-            <li>Eighty Seven</li>
-            <li>Ninety Eight</li>
-          </div>
-          <div className={scss.nav}>
-            <h3>Column Three</h3>
-            <li>One Two</li>
-            <li>Three Four</li>
-            <li>Five Six</li>
-            <li>Seven Eight</li>
-          </div>
-          <div className={scss.nav}>
-            <h3>Column Four</h3>
-            <div className={scss.btns}>
-              <button className={scss.app}>
-                <Image src={appStore} alt="img" />
-              </button>
-              <button className={scss.google}>
-                <Image src={googlePlay} alt="img" />
-              </button>
+    <>
+      <footer className={scss.Footer}>
+        <div className="container">
+          <div className={scss.content}>
+            <div className={scss.nav}>
+              <h3>Column One</h3>
+              <li>Twenty One</li>
+              <li>Thirty Two</li>
+              <li>Fourty Three</li>
+              <li>Fifty Four</li>
             </div>
-            <h3>Join Us</h3>
-            <div className={scss.icons}>
-              <a href="">
-                <FaYoutube />
-              </a>
-              <a href="">
-                <FaFacebookF />
-              </a>
-              <a href="">
-                <FaTwitter />
-              </a>
-              <a href="">
-                <FaInstagram />
-              </a>
-              <a href="">
-                <FaLinkedinIn />
-              </a>
+            <div className={scss.nav}>
+              <h3>Column Two</h3>
+              <li>Sixty Five</li>
+              <li>Seventy Six</li>
+              <li>Eighty Seven</li>
+              <li>Ninety Eight</li>
+            </div>
+            <div className={scss.nav}>
+              <h3>Column Three</h3>
+              <li>One Two</li>
+              <li>Three Four</li>
+              <li>Five Six</li>
+              <li>Seven Eight</li>
+            </div>
+            <div className={scss.nav}>
+              <h3>Column Four</h3>
+              <div className={scss.btns}>
+                <button className={scss.app}>
+                  <Image src={appStore} alt="img" />
+                </button>
+                <button className={scss.google}>
+                  <Image src={googlePlay} alt="img" />
+                </button>
+              </div>
+              <h3>Join Us</h3>
+              <div className={scss.icons}>
+                <a href="">
+                  <FaYoutube />
+                </a>
+                <a href="">
+                  <FaFacebookF />
+                </a>
+                <a href="">
+                  <FaTwitter />
+                </a>
+                <a href="">
+                  <FaInstagram />
+                </a>
+                <a href="">
+                  <FaLinkedinIn />
+                </a>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </footer>
+      </footer>
+      <footer className={scss.footer_mobile}>
+        <div className="container">
+          <div className={scss.btns}>
+            <button>
+              <FiMenu
+                style={{
+                  fontSize: "30px",
+                }}
+              />
+              Меню
+            </button>
+            <button>
+              <GoHome
+                style={{
+                  fontSize: "30px",
+                }}
+              />
+              Главный
+            </button>
+            <button>
+              <FaRegUserCircle
+                style={{
+                  fontSize: "30px",
+                }}
+              />
+              Профиль
+            </button>
+          </div>
+        </div>
+      </footer>
+    </>
   );
 };
 
